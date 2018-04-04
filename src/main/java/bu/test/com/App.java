8f5157ee3f8e75158d562ab31b1c2812a17c737d
shinkaryuk <Nord1disc@ukr.net>
2018-04-04 11:20:57 +0300

    A and app changed

--- src/main/java/bu/test/com/App.java
+++ src/main/java/bu/test/com/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         //
-        System.out.println( "Hello World!22222222222" );
+        System.out.println( "Hello World!22222222222333" );
     }
 }
